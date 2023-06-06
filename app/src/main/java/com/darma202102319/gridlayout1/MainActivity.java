c018dd5cb0084979a51973850e661a99409542e8
Darma Wirayuda <92357380+drmwryd@users.noreply.github.com>
2023-06-06 07:26:25 +0700

    Initial commit

---
+++ app/src/main/java/com/darma202102319/gridlayout1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.darma202102319.gridlayout1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
